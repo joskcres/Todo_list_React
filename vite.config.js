f24c4686b7b2dadf34988cf5c651b96bcb5363aa
Josué Cáceres <joskcres2@gmail.com>
2026-07-13 15:08:17 -0600

    Estructura final del proyecto y agregandolo a git hub pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react(),svgr()],base: '/Todo_list_React/',
 })
